5fb8f6e8fe888f68c51501c43e89fef4222ddf19
danielwsilva <danielwllace@gmail.com>
2022-12-21 10:24:20 -0300

    Day component created

---
+++ src/modules/purchase/components/Day/index.tsx
@@ -0,0 +1,24 @@
+import { TouchableOpacity } from 'react-native';
+
+import { Text } from '../../../../components';
+
+import { getStyles } from './styles';
+
+type DayProps = {
+  date: number | undefined;
+  isActive: boolean;
+  disabled: boolean;
+  onPress: () => void;
+};
+
+export const Day = ({ date, isActive, disabled, onPress }: DayProps) => {
+  const styles = getStyles(isActive, disabled);
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Text fontSize={12} fontWeight="normal" style={styles.text}>
+        {date}
+      </Text>
+    </TouchableOpacity>
+  );
+};
