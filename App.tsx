6f72ea5c50312e898df50b1348da77ac8eb57d89
danielwsilva <danielwllace@gmail.com>
2022-12-23 23:32:17 -0300

    Ticket component created

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { useFonts, Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
+import { useFonts, Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { Purchase } from 'modules/purchase/screens';
+import PurchaseStack from 'modules/purchase/routes/PurchaseStack';
+
 import 'config/ReactotronConfig';
-import { CartProvider } from 'hooks/cart';
 
 const queryClient = new QueryClient();
 
@@ -22,9 +24,11 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <CartProvider>
-          <Purchase />
-        </CartProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <NavigationContainer>
+            <PurchaseStack />
+          </NavigationContainer>
+        </GestureHandlerRootView>
       </SafeAreaProvider>
     </QueryClientProvider>
   );
