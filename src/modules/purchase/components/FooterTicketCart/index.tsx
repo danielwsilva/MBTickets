778431bbb6eb388d371bf7315a0b1bd207d1e46a
danielwsilva <danielwllace@gmail.com>
2022-12-23 23:33:17 -0300

    FooterTicketCart component created

---
+++ src/modules/purchase/components/FooterTicketCart/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Feather } from '@expo/vector-icons';
+
+import { Text } from 'components';
+import { TicketResponse } from 'services/api/purchase/types';
+import theme from 'styles/theme';
+
+import styles from './styles';
+import { TouchableOpacity, View } from 'react-native';
+import { useCart } from 'hooks/cart';
+
+type FooterTicketPurchaseProps = {
+  data: TicketResponse;
+}
+
+export const FooterTicketCart = ({ data }: FooterTicketPurchaseProps) => {
+  const [amount, setAmount] = useState(data.amount);
+  const { updateProduct } = useCart();
+
+  const { colors } = theme;
+
+  const increment = () => {
+    const amount = updateProduct(data, data.amount + 1);
+    setAmount(amount);
+  };
+
+  const decrement = () => {
+    const amount = updateProduct(data, data.amount - 1);
+    setAmount(amount);
+  };
+
+  const total = (data.price * amount).toFixed(2).replace('.', ',');
+
+  return (
+    <>
+      <View style={styles.wrapper}>
+        <TouchableOpacity style={{ ...styles.buttonMinus, backgroundColor: colors.black }} onPress={decrement}>
+          <Feather name="minus" size={20} color={colors.white} />
+        </TouchableOpacity>
+        <Text style={styles.text}>{amount}</Text>
+        <TouchableOpacity style={{ ...styles.buttonPlus, backgroundColor: colors.none }} onPress={increment}>
+          <Feather name="plus" size={20} color={colors.black} />
+        </TouchableOpacity>
+      </View>
+      <Text fontSize={14}>{`R$ ${total}`}</Text>
+    </>
+  );
+};
--- src/modules/purchase/components/index.ts
+++ src/modules/purchase/components/index.ts
@@ -2,3 +2,4 @@ export { CountCart } from './CountCart';
 export { Day } from './Day';
 export { Ticket } from './Ticket';
 export { FooterTicketPurchase } from './FooterTicketPurchase';
+export { FooterTicketCart } from './FooterTicketCart';
