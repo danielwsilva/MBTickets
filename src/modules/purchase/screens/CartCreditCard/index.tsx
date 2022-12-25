c9870aec90c7d2bcd353fbbcf132a58f1e7a69c3
danielwsilva <danielwllace@gmail.com>
2022-12-25 16:15:13 -0300

    Added new end-point

--- src/modules/purchase/screens/CartCreditCard/index.tsx
+++ src/modules/purchase/screens/CartCreditCard/index.tsx
@@ -1,37 +1,54 @@
 import { useState } from 'react';
 import { Platform, View, ScrollView } from 'react-native';
 import { Masks } from 'react-native-mask-input';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { AntDesign } from '@expo/vector-icons';
+import { CommonActions, useNavigation } from '@react-navigation/native';
+import { useQueryClient } from '@tanstack/react-query';
 import { Formik } from 'formik';
 import { v4 } from 'uuid';
 
-import { Button, Input, Text, Wrapper, CreditCard } from 'components';
+import { Button, Input, Text, Wrapper, CreditCard, Modal } from 'components';
 import { cardFlagKey, validityCardCredMask } from 'utils/helpers';
 import svgs from 'components/Icon/svgs';
+import { useCart } from 'hooks/cart';
+import { ROUTES } from 'navigation/appRoutes';
+import { usePayment } from 'services/api';
+import { createMyTicket } from 'services/api/keys';
+import theme from 'styles/theme';
 
 import { CardInfo, initialValues, validate, validationSchema } from './form';
-import { usePayment } from 'services/api/purchase';
-import { useCart } from 'hooks/cart';
-import { CommonActions, useNavigation } from '@react-navigation/native';
-import { ROUTES } from 'navigation/appRoutes';
+import styles from './styles';
 
 export const CartCreditCard = () => {
   const [cardFlag, setCardFlag] = useState<keyof typeof svgs>('defaultCard');
-  const { cart, setCart } = useCart();
+  const [visible, setVisible] = useState(false);
 
+  const { cart, setCart } = useCart();
   const { mutate, isLoading } = usePayment();
   const { dispatch } = useNavigation();
+  const queryClient = useQueryClient();
+
+  const { colors } = theme;
 
   const onSubmit = (values: CardInfo) => {
     const item = { id: v4(), card: values, tickets: cart };
     mutate(item, {
       onSuccess() {
-        setCart([]);
-        dispatch(
-          CommonActions.reset({
-            index: 0,
-            routes: [{ name: ROUTES.PURCHASE_INITIAL }]
-          })
-        );
+        setVisible(true);
+
+        setTimeout(() => {
+          queryClient.invalidateQueries(createMyTicket());
+          setVisible(false);
+          setCart([]);
+
+          dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [{ name: ROUTES.PURCHASE_INITIAL }]
+            })
+          );
+        }, 3000);
       }
     });
   };
@@ -41,90 +58,101 @@ export const CartCreditCard = () => {
   };
 
   return (
-    <Wrapper title="Pagamento" styleContainer={{ paddingHorizontal: 16,  paddingBottom: 20 }}>
-      <Text style={{ textAlign: 'center' }}>Cadastre seu cartão de crédito para efetuar o pagamentos</Text>
+    <>
+      <Wrapper title="Pagamento" styleContainer={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Cadastre seu cartão de crédito para efetuar o pagamentos</Text>
 
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validate={validate}
-        validationSchema={validationSchema}
-        validateOnChange={false}
-        validateOnBlur={false}
-      >
-        {({ handleChange, handleSubmit, values, errors, setErrors }) => (
-          <>
-            <ScrollView showsVerticalScrollIndicator={false}>
-              <CreditCard
-                cardNumber={values.number}
-                expireDate={values.exp_date}
-                cardFlag={cardFlag}
-                cardName={values.holder}
-                cardCvv={values.cvv}
-              />
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validate={validate}
+          validationSchema={validationSchema}
+          validateOnChange={false}
+          validateOnBlur={false}
+        >
+          {({ handleChange, handleSubmit, values, errors, setErrors }) => (
+            <>
+              <ScrollView showsVerticalScrollIndicator={false}>
+                <CreditCard
+                  cardNumber={values.number}
+                  expireDate={values.exp_date}
+                  cardFlag={cardFlag}
+                  cardName={values.holder}
+                  cardCvv={values.cvv}
+                />
 
-              <Input
-                placeholder="Número do cartão"
-                valid={errors.number === '' || !errors.number}
-                errorText={errors.number}
-                value={values.number}
-                onChangeText={handleChange('number')}
-                keyboardType={Platform.OS === 'android' ? 'numeric' : 'number-pad'}
-                mask={Masks.CREDIT_CARD}
-                onChange={(event) => {
-                  setErrors({ ...errors, number: '' });
+                <Input
+                  placeholder="Número do cartão"
+                  valid={errors.number === '' || !errors.number}
+                  errorText={errors.number}
+                  value={values.number}
+                  onChangeText={handleChange('number')}
+                  keyboardType={Platform.OS === 'android' ? 'numeric' : 'number-pad'}
+                  mask={Masks.CREDIT_CARD}
+                  onChange={(event) => {
+                    setErrors({ ...errors, number: '' });
 
-                  const card = cardFlagKey(event.nativeEvent.text) as keyof typeof svgs;
-                  setCardFlag(card);
-                }}
-                maxLength={19}
-              />
+                    const card = cardFlagKey(event.nativeEvent.text) as keyof typeof svgs;
+                    setCardFlag(card);
+                  }}
+                  maxLength={19}
+                />
 
-              <View style={{ flexDirection: 'row' }}>
-                <View style={{ flex: 1 }}>
-                  <Input
-                    placeholder="Validade"
-                    valid={errors.exp_date === '' || !errors.exp_date}
-                    errorText={errors.exp_date}
-                    onChangeText={handleChange('exp_date')}
-                    value={values.exp_date}
-                    keyboardType={Platform.OS === 'android' ? 'numeric' : 'number-pad'}
-                    mask={validityCardCredMask}
-                    onChange={() => setErrors({ ...errors, exp_date: '' })}
-                    maxLength={5}
-                  />
+                <View style={{ flexDirection: 'row' }}>
+                  <View style={{ flex: 1 }}>
+                    <Input
+                      placeholder="Validade"
+                      valid={errors.exp_date === '' || !errors.exp_date}
+                      errorText={errors.exp_date}
+                      onChangeText={handleChange('exp_date')}
+                      value={values.exp_date}
+                      keyboardType={Platform.OS === 'android' ? 'numeric' : 'number-pad'}
+                      mask={validityCardCredMask}
+                      onChange={() => setErrors({ ...errors, exp_date: '' })}
+                      maxLength={5}
+                    />
+                  </View>
+                  <View style={{ flex: 1, marginLeft: RFValue(20) }}>
+                    <Input
+                      placeholder="CVV"
+                      valid={errors.cvv === '' || !errors.cvv}
+                      errorText={errors.cvv}
+                      onChangeText={handleChange('cvv')}
+                      value={values.cvv}
+                      keyboardType={Platform.OS === 'android' ? 'numeric' : 'number-pad'}
+                      onChange={() => setErrors({ ...errors, cvv: '' })}
+                      maxLength={3}
+                    />
+                  </View>
                 </View>
-                <View style={{ flex: 1, marginLeft: 20 }}>
-                  <Input
-                    placeholder="CVV"
-                    valid={errors.cvv === '' || !errors.cvv}
-                    errorText={errors.cvv}
-                    onChangeText={handleChange('cvv')}
-                    value={values.cvv}
-                    keyboardType={Platform.OS === 'android' ? 'numeric' : 'number-pad'}
-                    onChange={() => setErrors({ ...errors, cvv: '' })}
-                    maxLength={3}
-                  />
-                </View>
-              </View>
 
-              <Input
-                placeholder="Nome como no cartão"
-                valid={errors.holder === '' || !errors.holder}
-                errorText={errors.holder}
-                onChangeText={handleChange('holder')}
-                value={values.holder}
-                onChange={() => setErrors({ ...errors, holder: '' })}
-                maxLength={64}
-              />
-            </ScrollView>
+                <Input
+                  placeholder="Nome como no cartão"
+                  valid={errors.holder === '' || !errors.holder}
+                  errorText={errors.holder}
+                  onChangeText={handleChange('holder')}
+                  value={values.holder}
+                  onChange={() => setErrors({ ...errors, holder: '' })}
+                  maxLength={64}
+                />
+              </ScrollView>
 
-            <Button disabled={disabled(values) || isLoading} loading={isLoading} onPress={() => handleSubmit()} >
+              <Button disabled={disabled(values) || isLoading} loading={isLoading} onPress={() => handleSubmit()}>
                 Finalizar pagamento
-            </Button>
-          </>
-        )}
-      </Formik>
-    </Wrapper>
+              </Button>
+            </>
+          )}
+        </Formik>
+      </Wrapper>
+
+      <Modal visible={visible} height={500}>
+        <View style={styles.modal}>
+          <AntDesign name="checkcircle" size={180} color={colors.success} />
+          <Text fontWeight="bold" fontSize={24} color={colors.success} style={styles.modalText}>
+            {'Compra realizada\n com sucesso'}
+          </Text>
+        </View>
+      </Modal>
+    </>
   );
 };
